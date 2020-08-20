c978f1f7df13b26d8deda30c782fe2a09c8a0042
Xujan24 <punsantosh1991@outlook.com>
2020-08-20 11:27:28 +1000

    adjusted server.js file

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 
 app.use(express.static(__dirname + '/dist'));
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.APP_PORT || 8080);
 
 app.length('/*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/demo-editor/index.html'));
 });
 
 console.log(`Yay!! app is running like a cream.`);
